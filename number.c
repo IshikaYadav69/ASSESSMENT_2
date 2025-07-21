794297bdc16e0c7414ceff8324733ff5bd599265
unknown <ishika.yadav3@s.amity.edu>
2025-07-20 22:46:10 -0700

    C3

--- number.c
+++ number.c
@@ -7,3 +7,9 @@ int main() {
     scanf("%d", &number);
     if (number % 2 == 0)
         printf("%d is even.\n", number);
+    //use if else statement not edited
+    else
+        printf("%d is odd.\n", number);
+
+    return 0;
+}
